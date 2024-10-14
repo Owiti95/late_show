3a4b1e810e93af4c23ff0cb0be5c9c58939bf535
hannington owiti <hanningtonowiti@gmail.com>
2024-10-14 15:49:13 +0300

    tried migration

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shows.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.before_first_request
 def create_tables():
